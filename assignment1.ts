3eb2c31f1e829860850fd260260085408d02fdbb
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-05-08 10:56:46 +0600

    Problem 7 solved

--- assignment1.ts
+++ assignment1.ts
@@ -7,8 +7,6 @@
   formatString("Hello", true);
   formatString("Hello", false);
 
-
-
   const filterByRating = (items: { title: string; rating: number }[]): { title: string; rating: number }[] => {
     return items.filter((item) => item.rating >= 4);
   };
@@ -21,72 +19,62 @@
 
   filterByRating(books);
 
-
-
-
   const concatenateArrays = <T>(...arrays: T[][]): T[] => {
     return arrays.flat();
   };
 
   concatenateArrays([1, 2], [3, 4]);
 
-
-
-
-
   class Vehicle {
-   private make: string;
-   private year: number;
+    private make: string;
+    private year: number;
 
     constructor(make: string, year: number) {
       this.make = make;
       this.year = year;
     }
     getInfo(): string {
-        return `Make: ${this.make}, Year: ${this.year}`;
+      return `Make: ${this.make}, Year: ${this.year}`;
     }
   }
 
   class Car extends Vehicle {
-   private model: string;
+    private model: string;
     constructor(make: string, year: number, model: string) {
       super(make, year);
       this.model = model;
     }
-    getModel(): string{
-        return `Model: ${this.model}`;
+    getModel(): string {
+      return `Model: ${this.model}`;
     }
   }
 
   const myCar = new Car("Toyota", 2020, "Corolla");
-  myCar.getInfo(); 
-  myCar.getModel(); 
-  
+  myCar.getInfo();
+  myCar.getModel();
 
-
-  const processValue = (value: string | number) =>{
-    if(typeof value === 'string'){
-        return value.length;
-    }else{
-        return value*2;
+  const processValue = (value: string | number) => {
+    if (typeof value === "string") {
+      return value.length;
+    } else {
+      return value * 2;
     }
-  }
+  };
 
-  processValue("Hello")
-  processValue(10)
-
+  processValue("Hello");
+  processValue(10);
 
   interface Product {
     name: string;
     price: number;
   }
-  
+
   const getMostExpensiveProduct = (products: Product[]): Product | null => {
     if (products.length === 0) {
       return null;
     } else {
       let mostExpensiveProduct = products[0];
-  
+
       for (const product of products) {
         if (product.price > mostExpensiveProduct.price) {
           mostExpensiveProduct = product;
@@ -95,22 +83,45 @@
       return mostExpensiveProduct;
     }
   };
-  
+
   const products = [
     { name: "Pen", price: 10 },
     { name: "Notebook", price: 25 },
-    { name: "Bag", price: 50 }
+    { name: "Bag", price: 50 },
   ];
+
+  const emptyProduct: Product[] = [];
+
+  getMostExpensiveProduct(products);
+  getMostExpensiveProduct(emptyProduct);
+
+
+
+
   
-  const emptyProduct : Product[] = []
 
-  getMostExpensiveProduct(products)
-  getMostExpensiveProduct(emptyProduct)
+  enum Day {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday,
+  }
+
+  const getDayType = (day: Day): string => {
+    if (day === Day.Saturday || day === Day.Sunday) {
+      return "Weekend";
+    } else {
+      return "Weekday";
+    }
+  };
+
+  getDayType(Day.Monday); 
+  getDayType(Day.Sunday); 
 
 
-
-
-  
 
 
 }
